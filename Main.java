b1326f682cca2e5dcd430726baa3f39e4d094429
Shriprada M N <133126805+shripradamn1@users.noreply.github.com>
2024-08-12 14:10:40 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+package main.java;
+
+public class Main {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        int result = main.add(5, 10);
+        System.out.println("Result: " + result);
+    }
+}
